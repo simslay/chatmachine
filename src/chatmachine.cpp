e42ae65aa4c102d307533e61f70313f074215a82
simslay <simon.grandsire@gmail.com>
2017-07-11 16:52:39 +0200

    Implement Version 2

--- src/chatmachine.cpp
+++ src/chatmachine.cpp
@@ -1,30 +1,51 @@
 //
 // Program Name: chatmachine
-// Description: a rudimentary chatbot in c++
+// Description: a chatbot in c++ using an AIML base
 //
 // Author: Simon Grandsire
 //
 
 #include "chatmachine.h"
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-string sInput = "";
-string sResponse = "";
+string basicAimlFiles[] = {
+    "bot",
+    "condition",
+    "default",
+    "random",
+    "setget",
+    "srai",
+    "star",
+    "that",
+    "think",
+    "topic"
+};
+
+size_t basicAimlFilesSize = sizeof(basicAimlFiles)/sizeof(basicAimlFiles[0]);
+
+string dataDir = "database/Basic/";
+
 string sUserPrompt = "USER> ";
 string sBotPrompt = "CHATMACHINE> ";
 
 int main()
 {
-    cout << "Chatmachine v1.0 Copyright (C) 2017 Simon Grandsire\n" << endl;
+    cout << "Chatmachine v2.0 Copyright (C) 2017 Simon Grandsire\n" << endl;
+
+    Chatmachine cm("Chatmachine");
+
+    cout << "Loading data..." << endl;
 
     while(1) {
         try {
-            listen();
-            respond();
+            cm.listen();
+            cm.respond();
         } catch(std::string message) {
             cerr << message << endl;
         } catch(...) {
@@ -35,23 +56,75 @@ int main()
     return 0;
 }
 
-void listen()
-{
-    cout << sUserPrompt;
-    getline(std::cin, sInput);
+void Chatmachine::init_random() {
+    srand((unsigned) time(NULL));
 }
 
-void respond() {
+void Chatmachine::listen() {
+    cout << sUserPrompt;
+    getline(cin, m_sInput);
+
+    if (m_sInput != "")
+        normalize(m_sInput);
+}
+
+void Chatmachine::respond() {
     string response;
 
-    if (sInput == "") {
-        std::cout << sBotPrompt << "Hmm." << std::endl;
+    init_random();
+
+    if (m_sInput == "") {
+        cout << sBotPrompt << "Hmm." << endl;
         return;
     }
 
-    if (response.empty()) {
-        cout << sBotPrompt <<  "I don't understand what you're saying." << std::endl;
-    } else {
-        std::cout << sBotPrompt << sResponse << std::endl;
+    if (m_sPrevInput != "" && m_sInput == m_sPrevInput) {
+        cout << sBotPrompt << "You have already said that." << endl;
+        return;
     }
+
+    response = get_response(m_sInput);
+
+    if (response.empty()) {
+        cout << sBotPrompt <<  "I don't understand what you're saying." << endl;
+    } else {
+        setResponse(response);
+
+        cout << sBotPrompt << m_sResponse << endl;
+    }
+}
+
+string Chatmachine::get_response(string input) {
+    string bestResponse;
+
+    bestResponse = get_best_response(input);
+    
+    return bestResponse;
+}
+
+string Chatmachine::get_best_response(string input) {
+    return "";
+}
+
+void Chatmachine::setResponse(string response) {
+    m_sResponse = response;
+    prepare_response(m_sResponse);
+
+    m_sPrevResponse = m_sResponse;
+    m_sPrevInput = m_sInput;
+}
+
+void Chatmachine::prepare_response(string &resp) {
+
+}
+
+void Chatmachine::normalize(string &input) {
+    //subsitute(input, subs);
+    //toUpper(input);
+    //shrink(input);
+    //insert_spaces(input);
+
+    //m_sInput = input;
+
+    //m_bInput_prepared = 1;
 }
--- src/chatmachine.h
+++ src/chatmachine.h
@@ -2,8 +2,41 @@
 #define __CHATMACHINE_H__
 
 #include <string>
+#include <vector>
 
-void listen();
-void respond();
+using namespace std;
+
+class Chatmachine {
+public:
+    Chatmachine (string str)
+        :m_sChatBotName(str), m_sInput(""), m_bInput_prepared(0), m_nFileIndex(0), m_sPrevResponse("")
+    {
+        init_random();
+    }
+
+    void listen();
+    void respond();
+
+private:
+    void init_random();
+    void normalize(string &input);
+    string get_response(string input);
+    string get_best_response(string input);
+    void setResponse(string sResponse);
+    void prepare_response(string &resp);
+
+private:
+    string m_sChatBotName;
+    string m_sInput;
+    string m_sResponse;
+    string m_sPrevInput;
+    string m_sPrevResponse;
+    string m_sSubject;
+    string m_sAimlFile;
+    vector<string> m_vsInputTokens;
+    bool m_bInput_prepared;
+    unsigned int m_nFileIndex;
+    vector<string> response_list;
+};
 
 #endif
